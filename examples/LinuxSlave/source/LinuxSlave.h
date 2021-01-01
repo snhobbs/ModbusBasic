391ecd9fa69d0b9a1c3cc7b6729238b2b6661de5
Simon <simon.hobbs@electrooptical.net>
2021-01-01 14:46:43 -0500

    factoring out some methods

--- examples/LinuxSlave/source/LinuxSlave.h
+++ examples/LinuxSlave/source/LinuxSlave.h
@@ -10,68 +10,19 @@
  */
 
 #pragma once
+#include "LinuxModbusTools.h"
+#include <Modbus/../../examples/posix/PosixSerial.h>
 #include <Modbus/Modbus.h>
-#include <IODevice/IODevice.h>
 #include <Modbus/BitControl.h>
 #include <Modbus/DataStore.h>
 #include <Modbus/DefaultSlaveDeviceIdentifier.h>
-#include <Modbus/ModbusRtu/ModbusRtuProtocol.h>
 #include <Modbus/ModbusRtu/ModbusRtuSlave.h>
 #include <Modbus/RegisterControl.h>
-#include <RingBuffer/RingBuffer.h>
 #include <Utilities/TypeConversion.h>
 #include <cassert>
 #include <cstdint>
-#include <cstdio> // standard input / output functions
-#include <cstdlib>
-#include <cstring> // string function definitions
-#include <fcntl.h> // File control definitions
-#include <iostream>
 #include <sys/time.h>
-#include <unistd.h>  // UNIX standard function definitions
-#include <Modbus/../../examples/posix/PosixSerial.h>
-//#include "../../posix/PosixSerial.h"
-#include <Utilities/Crc.h>
 #include <vector>
-#include <string>
-inline std::string GetFunctionName(const Modbus::Function function) {
-  switch (function) {
-  case Modbus::Function::kReadCoils:
-    return "Read Coils";
-  case Modbus::Function::kReadDiscreteInputs:
-    return "Read Discrete Inputs";
-  case Modbus::Function::kReadMultipleHoldingRegisters:
-    return "Read Multiple Holding Registers";
-  case Modbus::Function::kReadInputRegisters:
-    return "Read Input Registers";
-  case Modbus::Function::kWriteSingleCoil:
-    return "Read Single Coil";
-  case Modbus::Function::kWriteSingleHoldingRegister:
-    return "Write Single Holding Register";
-  case Modbus::Function::kWriteMultipleHoldingRegisters:
-    return "Write Multiple Holding Registers";
-  case Modbus::Function::kWriteMultipleCoils:
-    return "Write Multiple Coils";
-  case Modbus::Function::kReadDeviceIdentification:
-    return "Read Device Information";
-  default:
-    break;
-  }
-  char buff[256]{};
-  snprintf(buff, sizeof(buff), "Unknown Command %d", static_cast<int>(function));
-  return buff;
-}
-
-
-int64_t GetMicroSecondsSince(const timeval &first, const timeval &second) {
-  return (second.tv_sec - first.tv_sec) * 1000000 +
-         (second.tv_usec - first.tv_usec);
-}
-
-inline uint16_t crc16(const ArrayView<uint8_t> &array, std::size_t length) {
-  assert(length <= array.size());
-  return Utilities::crc16(array.data(), length);
-}
 
 inline const constexpr std::size_t kCoilCount = 125;
 inline const constexpr std::size_t kRegisterCount = 1024;
